b54b7d7cec9a0f3c4eb9e5098ffe91c3ae35ba14
jeurce <49353631+jeurce@users.noreply.github.com>
2021-07-30 19:50:03 -0400

    First Commit

---
+++ ADAP-3D-OBJECT-DETECTION.py
@@ -0,0 +1,15 @@
+import os
+import math
+import numpy as np
+import multiprocessing as mp
+import pickle
+import time
+from GET_OBJECT_IMGS import get_image_for_blocks
+import pandas as pd
+import logging
+
+
+if __name__ == '__main__':
+    get_image_for_blocks(r"C:\Users\jerry\Desktop\DCSM_PROFILE.mzML", r"C:\Users\jerry\Desktop\Results", window_mz = 8, window_rt= 16)
+    breakpoint()
+
