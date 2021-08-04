77c4a59924c73a9ab4520dcdea060285ebd84f95
jeurce <49353631+jeurce@users.noreply.github.com>
2021-08-03 23:11:14 -0400

    functional yolo model inferences with txt bounding box output

--- ADAP-3D-OBJECT-DETECTION.py
+++ ADAP-3D-OBJECT-DETECTION.py
@@ -1,18 +1,10 @@
 import os
-import math
-import numpy as np
-import multiprocessing as mp
-import pickle
-import time
 from GET_OBJECT_IMGS import get_image_for_blocks
-import pandas as pd
-import logging
-
 
 if __name__ == '__main__':
-    if not os.path.isdir(r'.\Blocks'):
+    if not os.path.isdir(r'Blocks'):
         os.system('mkdir .\Blocks')
-    get_image_for_blocks(r"C:\Users\jerry\Desktop\DCSM_PROFILE.mzML", r"C:\Users\jerry\Desktop\Results", window_mz = 25, window_rt= 65)
-    os.chdir("/yolov5")
-    os.system("python detect.py --weights /best.pt --img 416 --conf 0.4 --source ../Blocks")
+    #get_image_for_blocks(r"C:\Users\jerry\Desktop\DCSM_PROFILE.mzML", r"C:\Users\jerry\Desktop\Results", window_mz = 25, window_rt= 65)
+    os.chdir(r"yolov5")
+    os.system("python detect.py --weights C:\\Users\\jerry\\Desktop\\ADAP-3D-V2\\best.pt --img 416 --conf 0.4 --source C:\\Users\\jerry\\Desktop\\ADAP-3D-V2\\Blocks --save-txt")
 
